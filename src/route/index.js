19fd2e60e6bda1ce8a0b49f8a73b94657f4c6670
ieronimlatynnyk <ieronimlatynnyk@gmail.com>
2023-05-11 22:15:57 +0300

    upd master

--- src/route/index.js
+++ src/route/index.js
@@ -1507,7 +1507,7 @@ router.get('/shophome', function (req, res) {
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('shophome', {
     layout: 'shop',
-    
+
     navigation: {
       links: [
         {
@@ -1549,14 +1549,14 @@ router.get('/shophome', function (req, res) {
             'A new study published in the Journal of the American Medical Association finds that practicing mindfulness meditation can lead to improved mental health outcomes for people struggling with depression and anxiety.',
           isTop: false,
           isNew: false,
-        },
+          },
         {
           title: 'NASA Launches New Mars Rover',
           description:
             "NASA's Perseverance rover successfully launches on a mission to explore Mars, with the goal of collecting samples of the planet's surface and searching for signs of ancient microbial life.",
           isTop: false,
           isNew: true,
-        },
+          },
         {
           title:
             'GameStop Shares Soar as Reddit Traders Rally',
@@ -1564,7 +1564,7 @@ router.get('/shophome', function (req, res) {
             'Shares of GameStop surge as amateur traders on the social media platform Reddit rally to drive up the price, in what some are calling a battle between Wall Street and Main Street.',
           isTop: false,
           isNew: false,
-        },
+          },
         {
           title:
             'UK Announces Plan to Ban Sale of Gas-Powered Cars by 2030',
@@ -1572,24 +1572,24 @@ router.get('/shophome', function (req, res) {
             'In an effort to combat climate change, the UK government announces a plan to ban the sale of new gas-powered cars and vans by 2030, with hybrid vehicles to follow in 2035.',
           isTop: true,
           isNew: false,
-        },
-        {
+          },
+          {
           title:
             'New Study Shows Link Between Exercise and Longevity',
           description:
             'A new study published in the Journal of the American Medical Association suggests that regular exercise can help people live longer, with participants who exercised regularly having a lower risk of premature death.',
           isTop: false,
           isNew: false,
-        },
-        {
+          },
+          {
           title: 'Amazon Expands Grocery Delivery Service',
           description:
             'Amazon announces an expansion of its grocery delivery service, with plans to offer free delivery to Prime members on orders over $35 and to expand its selection of fresh and organic produce.',
           isTop: false,
           isNew: true,
-        },
-      ],
-    },
+          },
+        ],
+      },
 
     goodsBlock: {
       tabs: [
@@ -1647,15 +1647,15 @@ router.get('/shophome', function (req, res) {
           isHot: false,
           isNew: true,
         },
-        {
+          {
           image: 'https://picsum.photos/400/200',
           title: 'Nintendo Switch OLED Model',
           description:
             'The latest iteration of the popular gaming console features a larger OLED screen and improved audio quality.',
           isHot: false,
           isNew: false,
-        },
-        {
+          },
+          {
           image: 'https://picsum.photos/400/200',
           title: 'Bose SoundLink Revolve+',
           description:
@@ -1663,8 +1663,8 @@ router.get('/shophome', function (req, res) {
           isHot: true,
           isNew: true,
         },
-      ],
-    },
+          ],
+        },
     subscribe: {
       header: 'Unlock Premium Content',
       description:
@@ -1673,13 +1673,13 @@ router.get('/shophome', function (req, res) {
         {
           text: 'Register Now',
           link: 'https://www.youtube.com/',
-        },
+      },
         {
           text: 'Buy Subscription',
           link: 'https://www.youtube.com/',
-        },
-      ],
     },
+        ],
+      },
     service: {
       title: 'Our Services',
       description:
@@ -1688,10 +1688,377 @@ router.get('/shophome', function (req, res) {
         {
           text: 'Show More',
           link: 'https://www.youtube.com/',
+      },
+        ],
+      },
+
+    footer: [
+      [
+        {
+          text: 'Home',
+          link: 'https://www.youtube.com/',
+        },
+        {
+          text: 'Trending',
+          link: 'https://www.youtube.com/feed/trending',
+        },
+        {
+          text: 'Subscriptions',
+          link: 'https://www.youtube.com/feed/subscriptions',
+        },
+        {
+          text: 'Library',
+          link: 'https://www.youtube.com/feed/library',
+        },
+      ],
+      [
+        {
+          text: 'History',
+          link: 'https://www.youtube.com/feed/history',
+        },
+        {
+          text: 'Your Videos',
+          link: 'https://www.youtube.com/feed/my_videos',
+        },
+        {
+          text: 'Live',
+          link: 'https://www.youtube.com/live',
+        },
+        {
+          text: 'Settings',
+          link: 'https://www.youtube.com/account',
+        },
+      ],
+      [
+        {
+          text: 'Watch Later',
+          link: 'https://www.youtube.com/playlist?list=WL',
+        },
+        {
+          text: 'Liked Videos',
+          link: 'https://www.youtube.com/playlist?list=LL',
+        },
+        {
+          text: 'Music',
+          link: 'https://www.youtube.com/music',
+        },
+        {
+          text: 'Gaming',
+          link: 'https://www.youtube.com/gaming',
+        },
+      ],
+      [
+        {
+          text: 'Sports',
+          link: 'https://www.youtube.com/channel/UCo_q6aOlvPH7M-j_XGWVgXg',
+        },
+        {
+          text: 'News',
+          link: 'https://www.youtube.com/news',
+        },
+        {
+          text: 'Fashion & Beauty',
+          link: 'https://www.youtube.com/channel/UC1x8rV_f-2yPpzlN0JWZXIQ',
+        },
+        {
+          text: 'Learning',
+          link: 'https://www.youtube.com/learning',
+        },
+      ],
+      [
+        {
+          text: 'Report History',
+          link: 'https://www.youtube.com/feed/history/report_history',
+        },
+        {
+          text: 'Help',
+          link: 'https://support.google.com/youtube/?hl=en',
+        },
+        {
+          text: 'Send Feedback',
+          link: 'https://support.google.com/youtube/answer/4347644?hl=en',
+        },
+        {
+          text: 'About',
+          link: 'https://www.youtube.com/about/',
+        },
+      ],
+    ],
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+router.get('/shoporder', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('shoporder', {
+    layout: 'shop',
+    
+    navigation: {
+      links: [
+        {
+          text: 'Home',
+          href: 'https://github.com/',
+        },
+        {
+          text: 'Contacts',
+          href: 'https://www.google.com/',
+        },
+        {
+          text: 'Help',
+          href: 'https://www.youtube.com/',
+        },
+        {
+          text: 'Exit',
+          href: null,
         },
       ],
     },
 
+    header: 'Оформлення замовлення',
+    contacts_data: {
+      title: 'Контактні дані',
+
+      buttons: [
+        {
+          text: 'Я новий покупець',
+          isPrimary: true,
+        },
+        {
+          text: 'Я постійний покупець',
+          isOutline: true,
+        },
+      ],
+      form: {
+        input: {
+          surname: {
+            label: 'Прізвище',
+            placeholder: 'Іванов',
+            caption: 'Введіть прізвище',
+          },
+          name: {
+            label: 'Ім’я',
+            placeholder: 'Іван',
+            caption: 'Введіть Ім’я',
+          },
+          phone: {
+            label: 'Мобільний телефон',
+            placeholder: '+38 (XXX) XXX XX XX',
+            caption: 'Введіть телефон',
+          },
+          email: {
+            label: 'Пошта',
+            placeholder: 'ivanivanov@ukr.net',
+            caption: 'Введіть Пошту',
+          },
+        },
+        switch: [
+          {
+            text: 'Отримувати інформацію про новинки',
+            isActive: false,
+          },
+          {
+            text: 'Отримувати інформацію про знижки',
+            isActive: true,
+          },
+        ],
+      },
+    },
+    action_block: [
+      { text: 'Швидке замовлення', isSuccess: true },
+      { text: 'Видалити', isDanger: true },
+      { text: 'Детальніше', isInfo: true },
+    ],
+    total: {
+      title: 'Всього',
+      item_list: [
+        { name: 'Ноут', price: '25 000 ₴' },
+        { name: 'Мишка', price: '25 000 ₴' },
+        { name: 'Клава', price: '25 000 ₴' },
+      ],
+      additional_info: [
+        { text: 'Всього товарів', value: '75 000 ₴' },
+        { text: 'Купони', value: 'Немає' },
+        { text: 'Доставка', value: 'Безкоштовно' },
+      ],
+      total_price: {
+        text: 'Всього товарів',
+        value: '75 000 ₴',
+      },
+      button: [
+        {
+          text: 'Підтвердити замовлення',
+          isPrimary: true,
+        },
+        {
+          text: 'Відмінити купони',
+          isOutline: true,
+        },
+      ],
+    },
+    delivery: {
+      title: 'Доставка',
+      select: [
+        {
+          label: 'Місто доставки',
+          placeholder: 'Оберіть місто доставки',
+          options: [
+            { value: '1', text: 'Київ' },
+            { value: '2', text: 'Харків' },
+            { value: '3', text: 'Одеса' },
+            { value: '4', text: 'Львів' },
+            { value: '5', text: 'Дніпро' },
+            { value: '6', text: 'Донецьк' },
+          ],
+        },
+        {
+          label: 'Район доставки',
+          placeholder: 'Оберіть район доставки',
+          options: [
+            { value: '1', text: 'Голосіївський' },
+            { value: '2', text: 'Оболонський' },
+            { value: '3', text: 'Печерський' },
+            { value: '4', text: 'Дарницький' },
+            { value: '5', text: 'Деснянський' },
+            { value: '6', text: 'Подільський' },
+          ],
+        },
+      ],
+      delivery_type: [
+        {
+          type: 'Самовивіз з відділення Нової пошти',
+        },
+        {
+          type: 'Кур’єром Нової пошти',
+        },
+        {
+          type: 'Самовивіз з магазину',
+        },
+        {
+          type: 'Доставка закордон',
+        },
+      ],
+      payment: {
+        title: 'Оплата',
+        list: [
+          {
+            type: 'Оплата при отриманні',
+            description: 'За тарифами перевізника',
+          },
+          {
+            type: 'Оплатити зараз',
+          },
+        ],
+      },
+      contact: {
+        title: 'Контактні дані отримувача',
+        surname: {
+          label: 'Прізвище',
+          placeholder: 'Іванов',
+          caption: 'Введіть прізвище',
+        },
+        name: {
+          label: 'Ім’я',
+          placeholder: 'Іван',
+          caption: 'Введіть Ім’я',
+        },
+        phone: {
+          label: 'Мобільний телефон',
+          placeholder: '+38 (XXX) XXX XX XX',
+          caption: 'Введіть телефон',
+        },
+        email: {
+          label: 'Пошта',
+          placeholder: 'ivanivanov@ukr.net',
+          caption: 'Введіть Пошту',
+        },
+      },
+      coment: {
+        label: 'Коментар',
+        placeholder: 'Я хочу, щоб',
+      },
+    },
+    coupon: {
+      title: 'Купони',
+      select: {
+        availabel_coupon: {
+          label: 'Наявні купони',
+          placeholder: 'Виберіть купон',
+          options: [
+            { value: '1', text: 'SEEYALATER' },
+            { value: '2', text: 'HOTEL10' },
+            { value: '3', text: 'DOWNLOAD25' },
+          ],
+        },
+        friend_coupon: {
+          label: 'Купони за запрошених друзів',
+          placeholder: 'Виберіть купон',
+          options: [
+            { value: '1', text: 'TRAVEL5' },
+            { value: '2', text: '75VACA' },
+            { value: '3', text: '150VACA' },
+          ],
+        },
+      },
+      add: {
+        friend: {
+          label: 'Запросіть друзів та отримайте ще купонів',
+          placeholder:
+            'Введіть контактні дані друзів через ;',
+          caption:
+            'Напишіть електронні адреси/телефон друзів через “;”',
+        },
+        colleague: {
+          label: 'Запросити колег',
+          placeholder:
+            'Введіть контактні дані колег через ;',
+          caption:
+            'Напишіть електронні адреси/телефон колег через “;”',
+        },
+      },
+    },
+    quiz: {
+      title: 'Домоможи краще нам тебе зрозуміти',
+      preference: {
+        title:
+          'Який тип обладнання ви зацікавлені в придбанні?',
+        options: [
+          'Смартфони',
+          'Ноутбуки',
+          'Планшети',
+          'ПК',
+          'Телевізори',
+        ],
+      },
+      your_expirience: {
+        label: 'Який у Вас досвід користування ПК',
+      },
+      purchase_frequency: {
+        label: 'Як часто ви купуєте обладнання?',
+        placeholder: 'Оберіть варіант',
+        options: [
+          { value: '1', text: 'Раз в тиждень' },
+          { value: '2', text: 'Раз в місяць' },
+          { value: '3', text: 'Раз в рік' },
+        ],
+      },
+      factors: {
+        title:
+          'Які з наведених нижче факторів є для вас важливими при купівлі обладнання?',
+        options: [
+          'Ціна',
+          'Репутація бренду',
+          'Якість',
+          'Особливості',
+          'Гарантія',
+          'Доступність',
+          'Обслуговування клієнтів',
+        ],
+      },
+    },
     footer: [
       [
         {
